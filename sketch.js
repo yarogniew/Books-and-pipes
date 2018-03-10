fe71703634f93d17589168d9965d8f151b14b7d6
yarogniew <1@yarogniew.net>
2018-03-10 21:09:33 +0100

    dodanie noise1D

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
-
 var osc;
 var playing = false;
 var x = 215;
 var y = 215;
-var step = 0.5;
+var step = 0;
+
+var xoff = 0.05;
+var xincrement = 0.5;
 
 function setup() {
   createCanvas(480, 480);
@@ -16,22 +18,27 @@ function setup() {
 }
 
 function draw() {
+  var n = noise(xoff)*5;
+  // With each cycle, increment xoff
+
+  //print(n);
+
   if (keyIsPressed) {
 
     if (keyCode == LEFT_ARROW) {
-      x=x-step;
+      x=x-step-n;
       osc1.amp(0.5, 0);
     }
     if (keyCode == RIGHT_ARROW) {
-      x=x+step;
+      x=x+step+n;
       osc1.amp(0.5, 0);
     }
     if (keyCode == UP_ARROW) {
-      y=y-step;
+      y=y-step-n;
       osc2.amp(0.8, 0);
     }
     if (keyCode == DOWN_ARROW) {
-      y=y+step;
+      y=y+step+n;
       osc2.amp(0.8, 0);
     }
   }
@@ -39,4 +46,5 @@ function draw() {
 
   osc1.amp(0.0, 0.1);
   osc2.amp(0.0, 0.1);
+    xoff += xincrement;
 }
