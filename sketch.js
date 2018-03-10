30f8fbc9d0e938572e4aac05c3df804de385a7a1
yarogniew <1@yarogniew.net>
2018-03-11 00:23:49 +0100

    zmiana wielkości kwadratu

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
+var szerEkranu = 600;
+var wysEkranu = 600;
+
 var osc;
-var playing = false;
-var x = 215;
-var y = 215;
-var step = 0;
+//var playing = false;
+var x = szerEkranu/2;
+var y = wysEkranu/2;
+var step = 0.3;
 
 var xoff = 0.05;
 var xincrement = 0.5;
 
-function setup() {
-  createCanvas(640, 640);
+var a = 38; // bok kwadratu
 
+function setup() {
+  createCanvas(szerEkranu, wysEkranu);
+  background(200);
+ 	//createCanvas(displayWidth, displayHeight);
   osc1 = new p5.Noise('white');
   osc2 = new p5.Noise('pink');
   osc1.amp(0, 0);
@@ -19,34 +25,63 @@ function setup() {
 }
 
 function draw() {
+
+  if (mouseIsPressed) {
+          background(200);
+          }
+
   var n = noise(xoff)*5;
   // With each cycle, increment xoff
 
-  //print(n);
 
+
+
+  //print(n);
+      //background(200);
   if (keyIsPressed) {
 
-    if (keyCode == LEFT_ARROW) {
-      x=x-step-n;
-      osc1.amp(0.5, 0);
-    }
-    if (keyCode == RIGHT_ARROW) {
-      x=x+step+n;
-      osc1.amp(0.5, 0);
-    }
-    if (keyCode == UP_ARROW) {
-      y=y-step-n;
-      osc2.amp(0.8, 0);
-    }
-    if (keyCode == DOWN_ARROW) {
-      y=y+step+n;
-      osc2.amp(0.8, 0);
-    }
+    if (keyCode == BACKSPACE && a<60)
+    {
+        a=a+1;
+      }
+      if (keyCode == TAB && a>10)
+      {
+          a=a-1;
+        }
+
+
+    if (keyCode == ESCAPE)
+      {
+        background(200);
+      }
+
+    if (keyCode == LEFT_ARROW && x>0 )
+      {
+        x=x-step-n;
+        osc1.amp(0.5, 0);
+      }
+    if (keyCode == RIGHT_ARROW && x<szerEkranu-38)
+      {
+        x=x+step+n;
+        osc1.amp(0.5, 0);
+      }
+    if (keyCode == UP_ARROW && y>0)
+      {
+        y=y-step-n;
+        osc2.amp(0.8, 0);
+      }
+    if (keyCode == DOWN_ARROW && y<wysEkranu-38)
+      {
+        y=y+step+n;
+        osc2.amp(0.8, 0);
+      }
   }
+
+
+  rectMode(CENTER);
+  //ustawienie srodka jako punktu odniesienia
   fill('white');
-  rect(x+n, y-n, 40, 40);
-  noFill();
-  rect(1,1,640-2, 640-2);
+  rect(x+n, y-n, a, a);
 
   osc1.amp(0.0, 0.1);
   osc2.amp(0.0, 0.1);
